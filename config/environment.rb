c41b5705d27825b5c956550d6d1529450dd858d1
dani <danigb@gmail.com>
2009-05-20 21:40:15 +0200

    better 404, RedCloth downgraded

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,9 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.middleware.use "CalcaxyOld", "#{RAILS_ROOT}/public/calcaxy_old"
   config.time_zone = 'UTC'
+
+  config.gem 'RedCloth', :version => "= 3.0.3"
+
 end
 
 
