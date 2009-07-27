b0d38c186b59df51f42aabef87dd26a25d435e16
dani <dani@mandarine.(none)>
2009-07-27 13:28:15 +0200

    bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   helper :admin
- filter_parameter_logging :password
+  filter_parameter_logging :password
 end
